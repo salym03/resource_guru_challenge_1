fa2051ffe03016d4a68af75a9b0b6d2d7f376318
Mario Saly <salymario03@gmail.com>
2022-01-25 20:29:53 +0100

    Handle non-array type of parameters in

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const assert = require('assert');
 */
 
 function flattenArray(array) {
+  if (!Array.isArray(array)) {
+    return null;
+  }
+
   return array.reduce((acc, current) => {
     if (Array.isArray(current)) {
       return [...acc, ...flattenArray(current)];
@@ -15,12 +19,17 @@ function flattenArray(array) {
   }, [])
 }
 
-// Tests
+/* ==== Tests ==== */
 assert.deepStrictEqual(flattenArray([]), [], 'Empty array');
 assert.deepStrictEqual(flattenArray([ 1, 2, 3, 4 ]), [ 1, 2, 3, 4 ], 'Simple array');
 assert.deepStrictEqual(flattenArray([ 1, [ 2, 3 ], 4 ]), [ 1, 2, 3, 4 ], 'Nested array (Depth = 1)');
 assert.deepStrictEqual(flattenArray([ 1, [ 2, [ 3 ], [ 4, [ 5 ], 6 ] ], 7 ]), [ 1, 2, 3, 4, 5, 6, 7 ], 'Nested array (Depth > 2)');
 assert.deepStrictEqual(flattenArray([ 1, [], 4 ]), [ 1, 4 ], 'Nested array (empty array inside)');
+
+assert.deepStrictEqual(flattenArray("Array"), null, 'Non-array parameter (String)');
+assert.deepStrictEqual(flattenArray(1), null, 'Non-array parameter (Number)');
+assert.deepStrictEqual(flattenArray({ a: 1 }), null, 'Non-array parameter (Object)');
+assert.deepStrictEqual(flattenArray([ [ 1 ], [ "string" ], [{ a: 1 }]]), [ 1, "string", { a: 1 } ], 'Array with different type of elements');
 
 assert.deepStrictEqual(flattenArray([ 1, [ 2, [ 3 ] ], 4 ]), [ 1, 2, 3, 4 ], 'Provided test case');
 
